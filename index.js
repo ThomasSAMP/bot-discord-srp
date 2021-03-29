e26efe7b3591639e1982de492e3973ef4ad92c25
ThomasSAMP <lol.loli@outlook.fr>
2021-03-29 17:10:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,47 +2,44 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const mysql = require('mysql')
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+const channel = bot.channels.cache.get('685250262721757216')
 
-// const con = mysql.createConnection({
-//     host: "anderts899.mysql.db",
-//     user: "anderts899",
-//     password: "iG73U7dhH",
-//     database: "anderts899"
-// })
+const con = mysql.createConnection({
+    host: process.env.SQL_HOST,
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASS,
+    database: process.env.SQL_DB
+})
 
-// con.connect(function(err) {
-//     console.log("MySQL connected!")
-// })
+con.connect(function(err) {
+    console.log("MySQL connected!")
+})
 
 bot.on("ready", async() => {
-    console.log("Bot is ready.")
-    bot.user.setStatus("dnd")
-    bot.user.setActivity("manger des daronnes")
 
-    const channel = bot.channels.cache.get('685250262721757216')
+    console.log('Le bot est activé.')
+    console.log(`Connecté en tant que ${bot.user.tag}!`);
 
-    channel.send('Salut salut')
+    bot.user.setStatus('dnd')
+    bot.user.setActivity('manger des daronnes')
 
-    // for (var i = 1; i > 0; i++) {
-    //     console.log('salut')
-    //     con.query('SELECT * FROM `users`', function(error, results, fields) {
-    //         //channel.send('ID: ' + results[0]['id'] + 'Name: ' + results[0]['name'] + 'Email: ' + results[0]['email'] + 'VIP: ' + results[0]['vip'])
-    //         //console.log(results[0]['id'])
-    //     })
-    //     await sleep(10000)
-    // }
-
-    // ------------------------------------------------------------------------------------------------------------------------------------------------------- //
-
-    bot.on('message', msg => {
-        if (msg.content === 'ping') {
-            msg.reply('Pong!');
-        }
-    });
+    setInterval(newSanction, 20000);
 
 })
+
+function newSanction() {
+    console.log('Nouvelle sanction!')
+    con.query('SELECT * FROM `sanctions`', function(error, results, fields) {
+        // channel.send('ID: ' + results[0]['id'] + 'Name: ' + results[0]['name'] + 'Email: ' + results[0]['email'] + 'VIP: ' + results[0]['vip'])
+        console.log(results[0]['id'])
+    })
+}
+
+
+bot.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
 
 bot.login(process.env.TOKEN)
